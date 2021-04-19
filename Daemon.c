663f322c632dd6b89276a97205b7aa647f7d4531
RodrigoRK7 <a00824488@itesm.mx>
2021-04-19 18:52:20 -0500

    Daemons template

---
+++ Daemon.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <string.h>
+
+static void daemonize()
+{
+    pid_t pid;
+    
+    /* Fork off the parent process */
+    pid = fork();
+    
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+    
+     /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+    
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+    
+    /* Catch, ignore and handle signals */
+    /*TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+    
+    /* Fork off for the second time*/
+    pid = fork();
+    
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+    
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+    
+    /* Set new file permissions */
+    umask(027);
+    
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/Users/Hector/progrAva");
+    
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+    
+    /* Open the log file */
+    openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
+}
+
+int main()
+{
+    printf("Starting daemonize\n");
+    daemonize();
+    FILE *fp= NULL;
+    fp = fopen ("Log.txt", "w+");
+    if (fp != NULL){
+        while (1)
+        {
+            //TODO: Insert daemon code here.
+            syslog (LOG_NOTICE, "First daemon running.");
+            fprintf(fp, "Logging to file\n");
+            fflush(fp);
+            sleep (20);
+        }
+    }
+    fclose(fp);
+    syslog (LOG_NOTICE, "First daemon terminated.");
+    closelog();
+    
+    return EXIT_SUCCESS;
+}
